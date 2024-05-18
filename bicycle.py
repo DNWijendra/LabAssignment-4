3d6f8a0302a08f7381dc4eed5d31b923c6abec53
Dinithi Wijendra <wijendradn.21@uom.lk>
2024-05-18 08:26:05 +0530

    Added few functions to the bicycle class

--- bicycle.py
+++ bicycle.py
@@ -3,3 +3,11 @@ class Bicycle:
         self.brand = brand
         self.model = model
         self.is_rented = False
+
+    def rent(self):
+        self.is_rented = True
+        return f"{self.brand} {self.model} is now rented."
+
+    def return_bike(self):
+        self.is_rented = False
+        return f"{self.brand} {self.model} has been returned."
